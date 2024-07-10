ea72716b9704fa80ce76878615355946ba79b632
MD-NUR-MOHAMMAD <nurmohammad20161994@gmail.com>
2024-07-10 12:58:34 +0600

    indes file modifi

--- v11-List-Rendering/app.js
+++ v11-List-Rendering/app.js
@@ -1,25 +1,31 @@
 var app = Vue.createApp({
-  data() {
-    return {
-      skills: [
-        { name: "HTML",experience: 10,education: "BSC", institution: "IDB",languange: "English" },
-        { name: "CSS",experience: 20,education: "MCA", institution: "IUBAT",languange: "Bangla" },
-        { name: "Javascript",experience: 15,education: "BCA", institution: "IDB",languange: "Hindi" },
-        { name: "Vue",experience: 12,education: "MCA", institution: "IUBAT",languange: "Latin" },
-
-      ],
-      newSkill: {}
-    };
-  },
-  methods: {
-    addSkill() {
-      this.skills.push({name: this.newSkill.name ,experience: this.newSkill.experience,education: this.newSkill.education, institution: this.newSkill.institution,languange: this.newSkill.languange});
-      this.newSkill = {};
+    data() {
+      return {
+        skills: [
+          { name: "HTML",experience: 10,education: "BSC", institution: "IDB",languange: "English" },
+          { name: "CSS",experience: 20,education: "MCA", institution: "IUBAT",languange: "Bangla" },
+          { name: "Javascript",experience: 15,education: "BCA", institution: "IDB",languange: "Hindi" },
+          { name: "Vue",experience: 12,education: "MCA", institution: "IUBAT",languange: "Latin" },
+  
+        ],
+        newSkill: {
+          name: "",
+          experience: "",
+          education: "",
+          institution: "",
+          languange: "",
+        }
+      };
     },
-    removeSkill(i) {
-      this.skills.splice(i, 1);
-    },
-  }
-});
-
-app.mount("#app");
+    methods: {
+      addSkill() {
+        this.skills.push(this.newSkill);
+        this.newSkill = { name: "", experience: "", education: "", institution: "", languange: "" };
+      },
+      removeSkill(i) {
+        this.skills.splice(i, 1);
+      },
+    }
+  });
+  
+  app.mount("#app");
